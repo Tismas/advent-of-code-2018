e3abf0243b144d00e92f5824edc3f569fae0c17f
Adam Golan <adam.golan@grupawp.pl>
2018-12-13 08:47:04 +0100

    day12 part2

--- day12/solution.py
+++ day12/solution.py
@@ -1,20 +1,33 @@
-padding = 30
+offset = 4
+iterations = 50000000000
 with open('./input.txt') as f:
-    pots = list('.'*padding + f.readline().split()[-1] + '.'*padding)
+    init_state = f.readline().split()[-1]
+    pots = '....' + init_state + '....'
     combinations = {}
     for line in [x.strip() for x in f.readlines()]:
         combination, output = line.split(' => ')
         combinations[combination] = output
 
-    for _ in range(20):
-      next_generation = pots[:]
-      for i in range(0, len(pots)):
-          combination = ''.join(pots[i-2:i+3])
-          next_generation[i] = combinations.get(combination, '.')
-      pots = next_generation
+    iteration = 1
+    while iteration <= iterations:
+        next_generation = list(pots)
+        for i in range(len(pots)):
+            combination = ''.join(pots[i-2:i+3])
+            next_generation[i] = combinations.get(combination, '.')
+        next_generation = ''.join(next_generation)
+        cut_offset = next_generation.find('#')
+        offset -= cut_offset - 4
+        next_pots = '....' + \
+            ''.join(
+                next_generation[cut_offset:next_generation.rfind('#')+1]) + '....'
+        if pots == next_pots:
+            offset -= (next_generation.find('#') - 4) * iterations - iteration
+            break
+        pots = next_pots
+        iteration += 1
 
     s = 0
     for i in range(len(pots)):
-      if pots[i] == '#':
-        s += (i-padding)
+        if pots[i] == '#':
+            s += (i-offset)
     print(s)
