d490cc05faeaf1bd73a539b43c12135c60f15eab
Adam Golan <adam.golan@grupawp.pl>
2018-12-14 15:40:19 +0100

    day14 part 1

---
+++ day14/solution.py
@@ -0,0 +1,12 @@
+recipes = '37'
+first_elf = 0
+second_elf = 1
+needed_recipes = 147061
+while len(recipes) < needed_recipes + 10:
+  recipes += str(int(recipes[first_elf]) + int(recipes[second_elf]))
+  first_elf += (1 + int(recipes[first_elf]))
+  first_elf %= len(recipes)
+  second_elf += (1 + int(recipes[second_elf]))
+  second_elf %= len(recipes)
+
+print recipes[needed_recipes:needed_recipes+10]
